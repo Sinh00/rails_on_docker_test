0948219880804ecff69384bac322ecc293386081
Sinh00 <msoirnthe@gmail.com>
2020-07-15 02:46:00 +0900

    javascriptの読み込み練習

--- app/javascript/packs/user/index.js
+++
@@ -1,9 +0,0 @@
-function output_alert() {
-  alert("jsファイルのアラートです。");
-};
-
-$(document).ready( function() {
-  $("#t_btn").click(function(){
-    alert("hello-test");
-  });
-});
--- app/javascript/users/alert.js
+++
@@ -1,3 +0,0 @@
-function output_alert() {
-    alert("jsファイルのアラートです。");
-};
